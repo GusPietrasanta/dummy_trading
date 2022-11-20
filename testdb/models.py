7e456989e233945e6b6b042767d437653dcf5508
GusPietrasanta <gusdsm@gmail.com>
2022-11-20 21:54:30 +1030

    Updated .env

--- testdb/models.py
+++ testdb/models.py
@@ -1,3 +1,16 @@
+""" Create your models here. """
+
+import uuid
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    """" A class for dummy-trading users """
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4,
+        editable=False)
+    username = models.CharField(max_length=40)
+    hash = models.CharField(max_length=60)
+    balance = models.DecimalField(default=10000.00, max_digits=8, decimal_places=2)
